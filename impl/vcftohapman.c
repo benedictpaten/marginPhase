94bf470c786097834f55493de2c9abad51b5167c
yoheirosen <yrosen@berkeley.edu>
2017-11-09 14:40:13 -0800

    speed testing with vcf data

--- impl/vcftohapman.c
+++ impl/vcftohapman.c
@@ -1,7 +1,9 @@
 #include <htslib/vcf.h>
 #include "interface.h"
+#include <stdlib.h>
+#include <time.h>
 
-int lh_indices_from_vcf(char* vcf_path, size_t ref_start, size_t ref_end, linearReferenceStructure** return_lr, haplotypeCohort** return_cohort) {
+int lh_indices_from_vcf_subset(char* vcf_path, size_t ref_start, size_t ref_end, linearReferenceStructure** return_lr, haplotypeCohort** return_cohort, size_t number) {
   vcfFile* cohort_vcf = vcf_open(vcf_path, "r");
   if (cohort_vcf == NULL) {
       return 0;
@@ -12,15 +14,50 @@ int lh_indices_from_vcf(char* vcf_path, size_t ref_start, size_t ref_end, linear
   bcf_hdr_t* cohort_hdr = bcf_hdr_read(cohort_vcf);
   bcf1_t* record = bcf_init1();
   
-  size_t number_of_haplotypes = bcf_hdr_nsamples(cohort_hdr) * 2;
+  size_t number_of_haplotypes;
+  size_t max_number = bcf_hdr_nsamples(cohort_hdr) * 2;
+  if(number > max_number) {
+    number_of_haplotypes = max_number;
+  } else {
+    number_of_haplotypes = number;
+  }
   size_t length = ref_end - ref_start;
+  
+  size_t* indices = malloc(number_of_haplotypes * sizeof(size_t));
+  
+  time_t t;
+  srand((unsigned) time(&t));
+  
+  size_t n = 0;
+  if(number_of_haplotypes != max_number) {
+    while(n < number_of_haplotypes) {
+      size_t candidate = rand() % max_number;
+      
+      int not_found = 1;
+      if(n != 0) {
+        for(size_t i = 0; i < n; i++) {
+          if(candidate == indices[i]) {
+            not_found = 0;
+          }
+        }
+      }
+      if(not_found == 1) {
+        indices[n] = candidate;
+        n++;
+      }
+    }
+  } else {
+    for(size_t i = 0; i < max_number; i ++) {
+      indices[i] = i;
+    }
+  } 
   
   linearReferenceStructure* reference = linearReferenceStructure_init_empty(ref_start);
   haplotypeCohort* cohort = haplotypeCohort_init_empty(number_of_haplotypes, reference);
   int built_initial_span = 0;
   
-  printf("building haplotype cohort matrix, progress: [");
-  fflush(stdout);
+  fprintf(stderr, "building haplotype cohort matrix, progress: [");
+  fflush(stderr);
   int stepsize = (ref_end - ref_start)/50;
   int progress;
   int steps;
@@ -44,8 +81,8 @@ int lh_indices_from_vcf(char* vcf_path, size_t ref_start, size_t ref_end, linear
         int32_t *gt_arr = NULL, ngt_arr = 0;
         int ngt = bcf_get_genotypes(cohort_hdr, record, &gt_arr, &ngt_arr);
         if(site_index >= 0) {
-          for(size_t i = 0; i < ngt; i++) {
-            int allele_index = bcf_gt_allele(gt_arr[i]);
+          for(size_t i = 0; i < number_of_haplotypes; i++) {
+            int allele_index = bcf_gt_allele(gt_arr[indices[i]]);
             char allele_value = record->d.allele[allele_index][0];
             haplotypeCohort_set_sample_allele(cohort, site_index, i, allele_value);
           }
@@ -57,8 +94,8 @@ int lh_indices_from_vcf(char* vcf_path, size_t ref_start, size_t ref_end, linear
           stepsmade = steps - laststep;
           laststep = steps;
           for(size_t i = 0; i < stepsmade; i++) {
-            printf("=");
-            fflush(stdout);
+            fprintf(stderr, "=");
+            fflush(stderr);
           }
         }
         free(gt_arr);
@@ -111,3 +148,8 @@ void get_interval_bounds(const char* str, int32_t* beg, int32_t* end) {
   }
   free(s);
 }
+
+int lh_indices_from_vcf(char* vcf_path, size_t ref_start, size_t ref_end, linearReferenceStructure** return_lr, haplotypeCohort** return_cohort) {
+  return lh_indices_from_vcf_2(vcf_path, ref_start, ref_end, return_lr, return_cohort, 10000);
+}
+
--- inc/vcftohapman.h
+++ inc/vcftohapman.h
@@ -4,6 +4,7 @@
 #include <interface.h>
 
 int lh_indices_from_vcf(char* vcf_path, size_t ref_start, size_t ref_end, linearReferenceStructure** return_lr, haplotypeCohort** return_cohort);
+int lh_indices_from_vcf_subset(char* vcf_path, size_t ref_start, size_t ref_end, linearReferenceStructure** return_lr, haplotypeCohort** return_cohort, size_t number);
 
 void get_interval_bounds(const char* str, int32_t* beg, int32_t* end);
 
