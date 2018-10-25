54fbe59d20bbfa85bfa2b85840da82172639f640
Trevor Pesout <tpesout@ucsc.edu>
2018-10-25 14:09:51 -0700

    fixing compilation issues

--- tests/polisherTest.c
+++ tests/polisherTest.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#include <multipleAligner.h>
 
 static char *nanoporeHmmFile = "./params/polish/threeStateNanopore.hmm";
 static char *repeatCountsModelFile = "./params/polish/log_prob_matrices_fasta_one_liners_2x_pseudocounts.txt";
