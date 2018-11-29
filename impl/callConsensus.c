5c3caad9af827b3f326c1c3a93a93f18fc174ae6
ryan <rlorigro@ucsc.edu>
2018-11-29 12:24:44 -0800

    updated call to rleString_expand in callConsensus top level fn

--- impl/callConsensus.c
+++ impl/callConsensus.c
@@ -41,11 +41,11 @@ char* callConsensus(int readNo, char *readArray[], char *reference) {
     Poa *poaRefined = poa_realignIterative(reads, NULL, rleReference->rleString, p->polishParams);
 
     // Now get a non-RLE (expanded) string
-    char *nonRLEConsensusString = expandRLEConsensus(poaRefined, rleStrings, p->polishParams->repeatSubMatrix);
+    RleString* rleConsensus = expandRLEConsensus(poaRefined, rleStrings, p->polishParams->repeatSubMatrix);
 
-//    printf("%s\n", nonRLEConsensusString);
+    char* nonRleString = rleString_expand(rleConsensus);
 
-    return nonRLEConsensusString;
+    return nonRleString;
 }
 
 
