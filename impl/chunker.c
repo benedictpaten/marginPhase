16f1a3947c0e8f9e3fa964276c1ee055d12aa419
Trevor Pesout <tpesout@ucsc.edu>
2019-01-10 16:21:57 -0800

    adding quality calculation to BCR rle copy ctor

--- impl/chunker.c
+++ impl/chunker.c
@@ -225,13 +225,39 @@ BamChunkRead *bamChunkRead_construct2(char *readName, char *nucleotides, uint8_t
     return r;
 }
 BamChunkRead *bamChunkRead_constructRLECopy(BamChunkRead  *read, RleString *rle) {
-    BamChunkRead *r = malloc(sizeof(BamChunkRead));
+    BamChunkRead *r = st_calloc(1, sizeof(BamChunkRead));
     r->readName = read->readName ==  NULL ? NULL : stString_copy(read->readName);
     r->nucleotides = stString_copy(rle->rleString);
     r->readLength = rle->length;
-    r->qualities = NULL; //TODO maybe put some exciting logic here?
     r->forwardStrand = read->forwardStrand;
     r->parent = read->parent;
+    r->qualities = NULL;
+
+    // calculate read qualities (if set)
+    //TODO unit test this
+    if (read->qualities != NULL) {
+        r->qualities = st_calloc(rle->length, sizeof(uint8_t));
+        int64_t rawPos = 0;
+        for (int64_t rlePos = 0; rlePos < rle->length; rlePos++) {
+            uint8_t min = UINT8_MAX;
+            uint8_t max = 0;
+            int64_t mean = 0;
+            for (int64_t repeatIdx = 0; repeatIdx < rle->repeatCounts[rlePos]; repeatIdx++) {
+                uint8_t q = read->qualities[rawPos];
+                min = (q < min ? q : min);
+                max = (q > max ? q : max);
+                mean += q;
+
+                rawPos++;
+            }
+            mean = mean / rle->repeatCounts[rlePos];
+            assert(mean <= UINT8_MAX);
+            // pick your favorite metric
+            //r->qualities[rlePos] = min;
+            //r->qualities[rlePos] = max;
+            r->qualities[rlePos] = (uint8_t) mean;
+        }
+    }
 
     return r;
 }
